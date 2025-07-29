b29bf8d52bd15416706e1db962ad7994093abaa2
Shruti Bawase <shrutibawase01@gmail.com>
2025-07-29 23:25:06 +0530

    comment added on variable.tf

--- modules/azurerm_vitual_machine/variable.tf
+++ modules/azurerm_vitual_machine/variable.tf
@@ -1,86 +1,85 @@
+// Variables for Network Interface configuration
 variable "nic_name" {
   description = "The name of the network interface"
   type        = string
-
 }
+
 variable "nic_location" {
-  description = "The name of the network interface"
-  type        = string
-}
-variable "nic_resource_group" {
-  description = "The name of the network interface"
+  description = "The location of the network interface"
   type        = string
 }
 
-//no need of id 
+variable "nic_resource_group" {
+  description = "The resource group of the network interface"
+  type        = string
+}
+
+// No need of id variables for NIC and PIP as data sources are used
 
 # variable "pip_id" {
 #   description = "The ID of the public IP address associated with the network interface"
 #   type        = string
-  
 # }
 # variable "nic_subnet_id" {
 #   description = "The ID of the subnet for the network interface"
 #   type        = string
-
 # }
 
-//data block ke variable for subnet 
+// Variables for subnet data block
 variable "f_subnet_name" {
-  description = "The name of the subnet"
+  description = "The name of the frontend subnet"
   type        = string
-  
 }
+
 # variable "b_subnet_name" {
-#   description = "The name of the subnet"
+#   description = "The name of the backend subnet"
 #   type        = string
-  
 # }
+
 variable "virtual_network_name" {
   description = "The name of the virtual network"
   type        = string
-  
 }
 
-//data block ke variable for public ip
+// Variables for public IP data block
 variable "name_of_public_ip" {
   description = "The name of the public IP address"
   type        = string
-  
 }
 
+// Variables for Virtual Machine configuration
 variable "vm_name" {
   description = "The name of the virtual machine"
   type        = string
-  
 }
+
 variable "vm_resource_group_name" {
   description = "The name of the resource group for the virtual machine"
   type        = string
-  
 }
+
 variable "vm_location" {
   description = "The location of the virtual machine"
   type        = string
-  
 }
+
 variable "vm_size" {
   description = "The size of the virtual machine"
   type        = string
   default     = "Standard_DS1_v2"
-  
 }
+
 # variable "vm_admin_username" {
 #   description = "The admin username for the virtual machine"
 #   type        = string
-#   }
+# }
 # variable "vm_admin_password" {
 #   description = "The admin password for the virtual machine"
 #   type        = string
 #   sensitive   = true // Sensitive information, should not be logged or displayed
 # }
 
-//data block se keyvault through username & password value fetch kare ge
+// Variables for Key Vault data block to fetch username & password
 variable "key_vault_name" {
   description = "Key Vault name"
   type        = string
@@ -101,6 +100,7 @@ variable "secret_pass" {
   type        = string
 }
 
+// Variables for VM image configuration
 variable "image_publisher" {
   description = "The publisher of the image for the virtual machine"
   type        = string
